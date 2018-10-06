c59822fa84351f94bbb89c625a47434155aa1267
Liiz Quintero <analiz14_q@hotmail.com>
2018-10-06 14:56:02 -0500

    Commit inicial

---
+++ Encapsular/EUsuario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsular
+{
+    public class EUsuario
+    {
+        private string email;
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+
+        private string password;
+    }
+}
